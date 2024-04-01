9e712a7448c81315c37793eee90e17e24035f20d
justaspetrauskas <petrauskas.justas@gmail.com>
2024-04-01 18:25:26 +0200

    added tailwind

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <!-- <NuxtWelcome /> -->
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
+import { resolve } from 'path';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  alias: {
+    '@': resolve(__dirname, '/')
+  },
+  devtools: { enabled: true },
+  css: ['~/assets/main.scss'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
+    }
+  }
+});
